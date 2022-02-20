1a2d85296f0fa663a3e7238f0821a42d856149a0
martiinusest <Martin.teedla@gmail.com>
2022-02-20 11:27:09 +0200

    v.1

---
+++ toronto/Assets/playerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerScript : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "my score:" + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "danger")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        if (collision.gameObject.tag == "coin")
+        {
+            score++;
+            scoreText.text = "my score:" + score;
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
---
+++ toronto/Assets/test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speed,0,0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    { if(collision.gameObject.tag=="Player")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
